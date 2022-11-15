7f69707bac5e7b4377a615db81589e079b009884
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-15 11:28:44 -0800

    lab10 add8

--- processing/app.py
+++ processing/app.py
@@ -71,27 +71,6 @@ def get_stats():
     logger.info("request get has made")
     if not os.path.isfile(DATA_FILE):
         logger.error("Statistics does not exist creating now")
-        conn = sqlite3.connect(app_config['datastore']['filename'])
-
-
-        c = conn.cursor()
-        c.execute('''
-                CREATE TABLE stats
-                (id INTEGER PRIMARY KEY ASC, 
-                avg_env_humidity_reading FLOAT NOT NULL,
-                nm_env_recived INTEGER NOT NULL,
-                avg_env_temp_reading FLOAT NOT NULL,
-                max_env_temp_reading FLOAT NOT NULL,
-                avg_res_elerity_reading FLOAT NOT NULL,
-                avg_res_water_reading FLOAT NOT NULL,
-                max_res_water_reading FLOAT NOT NULL,
-                nm_res_recived INTEGER NOT NULL,
-                last_updated VARCHAR(100) NOT NULL)
-                ''')
-
-        conn.commit()
-        conn.close()
-        logger.info("Statistics create complete")
         return "Statistics does not exist", 404
 
     session = DB_SESSION()
