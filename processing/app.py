070d4ace52e68a70bf054af7bf0a9dabaacae033
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-15 11:18:12 -0800

    lab10 add7

--- processing/app.py
+++ processing/app.py
@@ -44,6 +44,28 @@ DB_ENGINE = create_engine("sqlite:///%s" % app_config["datastore"]["filename"])
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 DATA_FILE=app_config['datastore']['filename']
+if not os.path.isfile(DATA_FILE):
+    conn = sqlite3.connect(app_config['datastore']['filename'])
+
+
+    c = conn.cursor()
+    c.execute('''
+            CREATE TABLE stats
+            (id INTEGER PRIMARY KEY ASC, 
+            avg_env_humidity_reading FLOAT NOT NULL,
+            nm_env_recived INTEGER NOT NULL,
+            avg_env_temp_reading FLOAT NOT NULL,
+            max_env_temp_reading FLOAT NOT NULL,
+            avg_res_elerity_reading FLOAT NOT NULL,
+            avg_res_water_reading FLOAT NOT NULL,
+            max_res_water_reading FLOAT NOT NULL,
+            nm_res_recived INTEGER NOT NULL,
+            last_updated VARCHAR(100) NOT NULL)
+            ''')
+
+    conn.commit()
+    conn.close()
+    logger.info("Statistics create complete")
 
 def get_stats():
     logger.info("request get has made")
