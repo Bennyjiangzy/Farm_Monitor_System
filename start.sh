d28a6a754075ba74d57b0912c4a50935c4eddf70
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-19 16:36:22 -0800

    tag 3

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ docker build -t audit audit_log/
 docker build -t process processing/
 docker build -t receiver receiver/
 docker build -t storage storage/
-docker build -t dashboard dashboard-ui/
+docker build -t dashboardlab dashboard-ui/
