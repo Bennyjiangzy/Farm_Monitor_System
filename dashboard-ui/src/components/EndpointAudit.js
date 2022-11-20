f1ed33c2e2fff789baec392ec156b63edff77b5f
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-19 16:21:52 -0800

    tag change

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -5,7 +5,7 @@ export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
-	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store  asdad
     const [index, setIndex] = useState(null);
     const getAudit = () => {
         fetch(`http://benny-3855-lab.westeurope.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
@@ -31,6 +31,7 @@ export default function EndpointAudit(props) {
     } else if (isLoaded === false){
         return(<div>Loading...</div>)
     } else if (isLoaded === true){
+        console.log("test")
         return (
             <div>
                 <h3>{props.endpoint}-{index}</h3>
