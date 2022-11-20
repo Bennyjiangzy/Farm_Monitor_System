eec5b906710e13b0e23b06616af1da609110e8bf
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-19 16:37:28 -0800

    tag 3

--- assign.sh
+++ assign.sh
@@ -2,5 +2,5 @@ docker build -t audit audit_log/
 docker build -t process processing/
 docker build -t receiver receiver/
 docker build -t storage storage/
-docker build -t dashboard:assign2 dashboard-ui/
+docker build -t dashboardassign2 dashboard-ui/
 docker build -t health healthcheck/
