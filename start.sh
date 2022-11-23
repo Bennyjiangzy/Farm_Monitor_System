371b6175cf209b64e1673a57b4c8365d5b454721
BennyJiangzy <Bennyjiangzy@gmail.com>
2022-11-23 10:49:40 -0800

    test

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-docker build -t audit audit_log/
-docker build -t process processing/
+docker build -t auditlog audit_log/
+docker build -t processing processing/
 docker build -t receiver receiver/
 docker build -t storage storage/
 docker build -t dashboardlab dashboard-ui/
